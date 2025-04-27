3c1828801390b513d79758ef0577fdec469e8de3
RskPowerPZ <rskplayz5@gmail.com>
2025-04-27 19:49:39 +0530

    Update jwtgen.py

--- jwtgen.py
+++ jwtgen.py
@@ -87,7 +87,7 @@ def get_single_response():
             "uid": uid,
             "status": "invalid",
             "message": "Wrong UID or Password. Please check and try again.",
-            "credit": "@uditanshu_sarkar"
+            "credit": "@uditanshu_sarkar&@DarkBhaiFan"
         }), 400
 
     game_data = my_pb2.GameData()
